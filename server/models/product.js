3422b9c607136c2fd1a6fb7adc5e13c037b67561
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-03-14 21:40:06 +0200

    create product schema

---
+++ server/models/product.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  timestamps: { type: Date, default: Date.now },
+});
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
