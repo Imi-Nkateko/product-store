ef05bef5825f4318fe7bbe2342026ed6fc8b08cb
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-03-14 21:39:14 +0200

    add database imports

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 // entry point for api
 
-import express from "express"
+import express from "express";
+import dotenvv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
-const app = express()
+dotenvv.config();
 
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+console.log(process.env.MONGO_URI);
 app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000")
-})
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});
