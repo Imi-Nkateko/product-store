72edaad9fa3a2cff397bee64d8d24160bc1d701c
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-03-14 22:08:12 +0200

    add PORT

--- server/server.js
+++ server/server.js
@@ -8,13 +8,14 @@ import productRoutes from "./routes/product.route.js";
 dotenvv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
 console.log(process.env.MONGO_URI);
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:" + PORT);
 });
